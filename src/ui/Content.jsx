80dcc8cc30f5ce876ae03297d9757e135e09d60a
dimKonstantinopoulos <dimkonstantinopoulos@gmail.com>
2024-12-16 21:04:56 +0200

    Optimize images

--- src/ui/Content.jsx
+++ src/ui/Content.jsx
@@ -2,25 +2,25 @@ import Project from "./Project";
 
 const projectArr = [{
     "name": "CrustWorthy",
-    "imgPath": "../crustworthyImg.png",
+    "imgPath": "../crustworthyImg.jpg",
     "description": "Simple App to order pizzas through a modern SPA web app, using an API to let you select, update, delete and order pizzas. No need to signup for a new user account, just insert your name and you are good to go.",
     "browserLink": "https://crustworthy.netlify.app/",
     "gitLink": "https://github.com/JimKonstantinopoulos/CrustWorthy"
 },{
     "name": "WorldWise",
-    "imgPath": "../../worldwiseImg.png",
+    "imgPath": "../../worldwiseImg.jpg",
     "description": "Track the cities and countries you have visited so far. Using an authentication system to log in, select each city via a user friendly environment, using a clickable map and store them into an interactive list with the ability to comment, delete and track each city back on the map.",
     "browserLink": "https://w0rldwise.netlify.app/",
     "gitLink": "https://github.com/JimKonstantinopoulos/WorldWise"
 },{
     "name": "Rate-n-Save",
-    "imgPath": "../../ratensaveImg.png",
+    "imgPath": "../../ratensaveImg.jpg",
     "description": "Search the movie you wish from a user friendly, authentication free UI, using a simple API, as well as rate and save them to keep track on them every time.",
     "browserLink": "https://rate-n-save.netlify.app/",
     "gitLink": "https://github.com/JimKonstantinopoulos/Rate-n-Save"
 },{
     "name": "Forkify",
-    "imgPath": "../../forkifyImg.png",
+    "imgPath": "../../forkifyImg.jpg",
     "description": "Find any recipe you need through a simple UI, by adding either the dish or even one of it's ingredients using a simple API. Dynamic recipes are also produced due to the number of servings option capability. Need more information? An additional link to each recipe window, will take you to a step by step cooking guide website for that specific recipe.",
     "browserLink": "https://forkify-j.netlify.app/",
     "gitLink": "https://github.com/JimKonstantinopoulos/Forkify"
@@ -29,7 +29,7 @@ const projectArr = [{
 function Content(){
     return <main className="bg-background2 flex flex-col justify-center items-center">
         <p className="text-stone-200 text-5xl desktop:text-7xl p-10">Take a look at my projects</p>
-        <div className="grid tablet_w:grid-cols-2 w-full justify-items-center gap-5 laptop:gap-10 p-10 max-w-[70rem]">
+        <div className="grid tablet_w:grid-cols-2 w-full justify-items-center gap-8 laptop:gap-10 p-10 max-w-[70rem]">
             {projectArr.map(project => <Project key={project.browserLink} project={project}/>)}
         </div>
     </main>
--- src/ui/Project.jsx
+++ src/ui/Project.jsx
@@ -4,7 +4,7 @@ import { SlGlobe } from "react-icons/sl";
 function Project({project}){
     return <div className="flex flex-col text-stone-800 tablet_w:max-w-[25rem] laptop:h-[30rem] bg-slate-200 rounded-lg overflow-hidden">
         <div className="relative">
-            <img src={project.imgPath} alt="project view" />
+            <img src={project.imgPath} alt="project view" loading="lazy" />
             <div className="absolute bottom-0 left-0 right-0 bg-gradient-to-t from-slate-200 to-transparent h-[90%]"></div>
         </div>
         <p className="font-bold text-lg pt-2 pl-3">{project.name}</p>
